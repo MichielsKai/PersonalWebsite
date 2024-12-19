d80072838662a799c037af35f1f16c7734bd0423
MichielsKai <Kai.Michiels.Work@gmail.com>
2024-12-19 10:24:06 +0100

    Finished the tutorial!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-    site: "https://kaimichiels.netlify.app/",
+  site: "https://kaimichiels.netlify.app/",
+  integrations: [preact()],
 });
